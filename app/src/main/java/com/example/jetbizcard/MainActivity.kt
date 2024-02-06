753ec201df0df6d28f4d8e3db0ad26c48556f50a
Mohd Mojiz Hasan <158858724+SyedRizvi786092@users.noreply.github.com>
2024-02-07 02:51:58 +0530

    The JetBizCard app has been successfully build for the first time...

--- app/src/main/java/com/example/jetbizcard/MainActivity.kt
+++ app/src/main/java/com/example/jetbizcard/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.jetbizcard
 
+//import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -15,30 +19,40 @@ import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
+//import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
-import androidx.compose.material3.CardDefaults.cardElevation
-import androidx.compose.material3.CardElevation
+import androidx.compose.material3.CardDefaults
+//import androidx.compose.material3.CardColors
+//import androidx.compose.material3.CardDefaults.cardElevation
+//import androidx.compose.material3.CardElevation
+//import androidx.compose.material3.CardElevation
 import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.contentColorFor
+//import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+//import androidx.compose.ui.graphics.painter.BitmapPainter
+//import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
+//import androidx.compose.ui.text.TextStyle
+//import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
+//import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.TextUnitType
@@ -73,7 +87,7 @@ fun CreateBizCard() {
             .height(390.dp)
             .padding(20.dp),
             shape = RoundedCornerShape(corner = CornerSize(25.dp)),
-            elevation = cardElevation(10.dp,4.dp,6.dp,8.dp,8.dp,20.dp)
+            elevation = CardDefaults.cardElevation()
         ) {
             Column(modifier = Modifier
                 .height(380.dp)
@@ -81,7 +95,7 @@ fun CreateBizCard() {
                 .padding(vertical = 5.dp),
                 verticalArrangement = Arrangement.Top,
                 horizontalAlignment = Alignment.CenterHorizontally) {
-                CreateProfileImage()
+                CreateProfileImage(modifier = Modifier)
                 Divider(modifier = Modifier.height(2.dp),
                     thickness = 10.dp,
                     color = Color.Gray)
@@ -94,6 +108,9 @@ fun CreateBizCard() {
 
 @Composable
 private fun ShowSkills() {
+    val buttonClickedState = remember {
+        mutableStateOf(false)
+    }
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -102,7 +119,9 @@ private fun ShowSkills() {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Button(
-            onClick = { /*TODO*/ },
+            onClick = {
+                      buttonClickedState.value = !buttonClickedState.value
+            },
             shape = RoundedCornerShape(18.dp),
             modifier = Modifier.size(width = 100.dp, height = 50.dp)
         ) {
@@ -113,6 +132,12 @@ private fun ShowSkills() {
                 fontSize = TextUnit(19.5f, TextUnitType.Sp)
             )
         }
+        if(buttonClickedState.value)
+            ListSkills()
+        else
+            Box {
+
+            }
     }
 }
 
@@ -120,7 +145,7 @@ private fun ShowSkills() {
 private fun DisplayText() {
     Text(
         text = "Mojiz Hasan",
-        color = Color.Yellow,
+        color = MaterialTheme.colorScheme.surfaceTint,
         fontSize = TextUnit(37.5f, TextUnitType.Sp),
         fontWeight = FontWeight.Bold,
         fontFamily = FontFamily.Serif,
@@ -141,9 +166,9 @@ private fun DisplayText() {
 }
 
 @Composable
-private fun CreateProfileImage() {
+private fun CreateProfileImage(modifier: Modifier) {
     Surface(
-        modifier = Modifier
+        modifier = modifier
             .size(250.dp)
             .padding(10.dp),
         shape = CircleShape,
@@ -154,9 +179,57 @@ private fun CreateProfileImage() {
         Image(
             painter = painterResource(id = R.drawable.profile_image),
             contentDescription = "Profile Image",
-            modifier = Modifier.size(135.dp),
+            modifier = modifier.size(135.dp),
             contentScale = ContentScale.FillBounds
         )
+    }
+}
+
+@Composable
+fun ListSkills() {
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .padding(5.dp)) {
+        Surface(modifier = Modifier
+            .fillMaxSize()
+            .padding(3.dp),
+            shape = RoundedCornerShape(15.dp),
+            border = BorderStroke(4.dp,Color.DarkGray)
+        ) {
+            Skills(data = listOf("Android Development", "Problem-Solving", "GitHub"))
+        }
+    }
+}
+
+@Composable
+fun Skills(data: List<String>) {
+    LazyColumn {
+        items(data) {
+            item -> Card(modifier = Modifier
+            .padding(10.dp)
+            .fillMaxWidth(),
+            shape = RectangleShape,
+            elevation = CardDefaults.cardElevation()
+            ) {
+                Row(modifier = Modifier
+                    .padding(5.dp).fillMaxWidth()
+                    .background(MaterialTheme.colorScheme.surface)) {
+                    CreateProfileImage(modifier = Modifier.size(100.dp))
+                    Column(modifier = Modifier
+                        .padding(2.dp)
+                        .align(alignment = Alignment.CenterVertically)) {
+                        Text(text = item,
+                            fontWeight = FontWeight.Bold,
+                            style = MaterialTheme.typography.titleMedium,
+                            color = MaterialTheme.colorScheme.primary
+                        )
+                        Text(text = "A great skill indeed!",
+                            fontWeight = FontWeight.Light,
+                            style = MaterialTheme.typography.bodySmall)
+                    }
+                }
+            }
+        }
     }
 }
 
